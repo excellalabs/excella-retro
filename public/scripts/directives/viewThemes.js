f553bc9cc78ee5fba5fae9c5f2bc47ed13152ea2
Doguhan Uluca <duluca@gmail.com>
2014-09-03 18:48:50 -0400

    voting logic for themes

--- public/scripts/directives/viewThemes.js
+++ public/scripts/directives/viewThemes.js
@@ -16,6 +16,34 @@ app.directive('viewThemes', [function() {
         controller: function($scope, boardService) {
             $scope.allowedVotes = helpers.dotVotesAllowed($scope.themes.length);
 
+            var usedVotes = 0;
+
+            function canUpVote() {
+                return usedVotes <= $scope.allowedVotes;
+            }
+
+            function canDownVote(themeVotes) {
+                return usedVotes > 0 && themeVotes > 0;
+            }
+
+            $scope.upVote = function(themeId) {
+                if(canUpVote()) {
+                    var theme = _.findWhere($scope.themes, {id: themeId});
+                    theme.votes++;
+                    usedVotes++;
+                    $scope.$apply();
+                }
+            };
+
+            $scope.downVote = function(themeId) {
+                var theme = _.findWhere($scope.themes, {id: themeId});
+                if(canDownVote(theme.votes)) {
+                    theme.votes--;
+                    usedVotes--;
+                    $scope.$apply();
+                }
+            };
+
             $scope.sendTheme = function() {
                 boardService.sendTheme($scope.boardId, $scope.theme).then(function(savedTheme) {
                 }, function(validation){
