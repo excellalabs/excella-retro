2c88e90865df18c1cb4395e630d33d9d0ccd7cae
Doguhan Uluca <duluca@gmail.com>
2014-10-17 01:10:19 -0400

    First pass on Summary page

--- public/scripts/directives/phases/summary.js
+++ public/scripts/directives/phases/summary.js
@@ -1,2 +1,21 @@
 /* global require, module, exports */
 /* jslint browser: true */
+
+var app = require('./../_module_init.js');
+var constants = require('../../../../shared/constants/boardConstants');
+
+app.directive('summary', [function() {
+    "use strict";
+    return {
+        restrict: 'E',
+        templateUrl: 'templates/directives/summary.html',
+        scope: {
+            board: '='
+        },
+        controller: ['$scope', function($scope) {
+            $scope.phases = constants.phases;
+        }]
+    };
+}]);
+
+module.exports = app;
