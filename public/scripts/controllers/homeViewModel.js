644d7bb2e004b6fdfdaea31d6eb09fbf457f0c83
Doguhan Uluca <duluca@gmail.com>
2014-10-20 00:50:36 -0400

    login modals are in place

--- public/scripts/controllers/homeViewModel.js
+++ public/scripts/controllers/homeViewModel.js
@@ -3,7 +3,7 @@
 
 var app = require('./_module_init.js');
 
-app.controller('HomeController', ['$scope', 'userProvider', 'boardService', '$location', '$rootScope', function($scope, userProvider, boardService, $location, $rootScope) {
+app.controller('HomeController', ['$scope', 'userProvider', 'boardService', '$location', '$rootScope', '$modal', function($scope, userProvider, boardService, $location, $rootScope, $modal) {
     "use strict";
 
     $scope.createBoard = function(){
@@ -20,7 +20,21 @@ app.controller('HomeController', ['$scope', 'userProvider', 'boardService', '$lo
 
         boardService.createBoard($scope.user, $scope.boardName, scrumMasterKey).then(function(board){
             $rootScope.boardId = board.id;
-            $location.path('retro');
+
+            var modalInstance = $modal.open({
+                templateUrl: 'templates/modal.html',
+                controller: 'ModalInstanceController',
+                size: 'sm',
+                resolve: {
+                    title: function() { return "New Retrospective"; },
+                    body: function() { return "As the Scrum Master you will control the phases of the retrospective on all participants\' devices. You may invite participants by sharing the invitation link or creating an e-mail. You must save the Admin access link to your computer, in case you get disconnected from this session, otherwise you won't be able to access this retrospective as an admin again. To delete your data off our servers, you must close the retrospective at the end."; },
+                    hasCancel: function() { return false; }
+                }
+            });
+
+            modalInstance.result.then(function (selectedItem) {
+                $location.path('retro');
+            });
         }, function(validation){
             if(typeof validation !== "object"){
                 validation = [validation];
