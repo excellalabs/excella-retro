c399f52f37fcb41032b4fa89c267ee8fc875d9d0
Doguhan Uluca <duluca@gmail.com>
2016-01-14 22:58:12 -0500

    switched database to mongodb

--- server/models/database.js
+++ server/models/database.js
@@ -1,13 +1,58 @@
 /* jslint node: true */
-var level = require('level');
-var path = require('path');
+//var level = require('level');
+//var path = require('path');
+//
+//var basePath = process.env.APP_DIR || ".";
+//
+//var dbPath = path.join(basePath, '/remoteRetroDb');
+//
+//console.log('Database path: ' + dbPath);
+//
+//var db = level(dbPath, { valueEncoding: 'json'});
 
-var basePath = process.env.APP_DIR || ".";
+var bluebird = require('bluebird');
+var mongodb = require('mongodb');
+var MongoClient = mongodb.MongoClient;
+var Collection = mongodb.Collection;
 
-var dbPath = path.join(basePath, '/remoteRetroDb');
+bluebird.promisifyAll(Collection.prototype);
+bluebird.promisifyAll(MongoClient);
 
-console.log('Database path: ' + dbPath);
+var url = process.env.MONGO_URI || 'mongodb://localhost:27017/RetroDb';
 
-var db = level(dbPath, { valueEncoding: 'json'});
+var dbPromise = MongoClient.connectAsync(url);
 
-module.exports = db;
+module.exports = {
+    get: function(key, cb) {
+        dbPromise.then(function(db) {
+           db.collection('retro').findOne({ key: key }, function(err, retro) {
+               if(err) {
+                   cb(err);
+               } else {
+                   cb(err, retro.value);
+               }
+           });
+        });
+    },
+    put: function(key, value, cb) {
+        dbPromise.then(function(db) {
+            db.collection('retro').update({ key: key }, {
+                key: key,
+                value: value
+            }, { upsert: true }, function(err, retro) {
+                if(err) {
+                    cb(err);
+                } else {
+                    cb(err, retro.value);
+                }
+            });
+        });
+    },
+    del: function(key, cb) {
+        dbPromise.then(function(db){
+            db.collection('retro').remove({ key: key }, function(err) {
+               cb(err);
+            });
+        });
+    }
+};
